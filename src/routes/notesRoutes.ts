ef35642b811a9443f1b47f3311861e980ed06ed5
Juan Camilo Velásquez Amarillo <camilojheans@gmail.com>
2023-12-03 21:48:53 -0500

    Update noteRoutes

--- src/routes/notesRoutes.ts
+++ src/routes/notesRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", notesController.getAllNotes);
 router.get("/:id", notesController.getNoteById);
 router.post("/", notesController.createNote);
-router.patch("/:id", notesController.updateNote);
+router.put("/:id", notesController.updateNote);
 router.delete("/:id", notesController.deleteNote);
 
 export default router;
